e668350296768601790fed58da75846af77a35ea
Aaron Gobeyn <aaron.gobeyn@gmail.com>
2024-10-09 09:55:34 +0200

    Removed the install Mistral line from the ollama install, if we use the llm script we've written, then Mistral will be installed automatically with the first use.

--- install/ollama-install.sh
+++ install/ollama-install.sh
@@ -2,11 +2,3 @@
 
 # Install ollama
 curl -fsSL https://ollama.com/install.sh | sh
-
-# Download Mistral, if ollama is installed
-if command -v ollama &> /dev/null 
-then
-  ollama run mistral
-fi
-
-
